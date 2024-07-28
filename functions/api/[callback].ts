76a0ab03442db1124d063f2be03d31bd55f2e2a6
green-dalii <654534332@qq.com>
2024-07-28 16:27:07 +0800

    move auth to Functions

--- functions/api/[callback].ts
+++ functions/api/[callback].ts
@@ -1,7 +1,52 @@
 import { Lucia } from "lucia";
 import { D1Adapter } from "@lucia-auth/adapter-sqlite";
-// import { D1Database } from "@cloudflare/workers-types";
+import { D1Database } from "@cloudflare/workers-types";
 import { GitHub } from "arctic";
+
+export const github = new GitHub(
+	env.GITHUB_ID,
+	env.GITHUB_SECRET
+);
+
+
+export function initializeLucia(D1: D1Database) {
+	const adapter = new D1Adapter(D1, {
+		user: "user",
+		session: "session"
+	});
+
+	const lucia = new Lucia(adapter, {
+        sessionCookie: {
+          attributes: {
+            secure: env.AUTH_SECRET
+          }
+        },
+        getUserAttributes: (attributes) => {
+          return {
+            // attributes has the type of DatabaseUserAttributes
+            githubId: attributes.github_id,
+            username: attributes.username
+          };
+        }
+      });
+    
+      return lucia;
+}
+
+// 定义用户属性接口
+interface DatabaseUserAttributes {
+	github_id: number;
+	username: string;
+}
+
+// 声明Lucia模块
+declare module "lucia" {
+    interface Register {
+      Lucia: ReturnType<typeof initializeLucia>;
+      DatabaseUserAttributes: DatabaseUserAttributes;
+    }
+  }
+
 
 export async function onRequest(context) {
     const { request, env } = context;
