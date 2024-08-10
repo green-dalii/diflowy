a9367203fd0bf0178b407bc55756df1519df93b5
green-dalii <654534332@qq.com>
2024-08-10 13:09:03 +0800

    fixed auth logical

--- src/components/flow/ReactFlowComponent.jsx
+++ src/components/flow/ReactFlowComponent.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react';
-import { ReactFlow, applyEdgeChanges, applyNodeChanges, Controls, Background, MiniMap, Panel } from '@xyflow/react';
+import { ReactFlow, applyEdgeChanges, applyNodeChanges, Background } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import '../../styles/workflow.css'
 
