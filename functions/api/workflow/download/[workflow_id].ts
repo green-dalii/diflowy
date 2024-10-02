70f5ccf432d42c42feb482a72dc8b2fa2d86c509
green-dalii <654534332@qq.com>
2024-10-02 21:29:58 +0800

    fix version display error in update page; fix import and download button actions

--- functions/api/workflow/download/[workflow_id].ts
+++ functions/api/workflow/download/[workflow_id].ts
@@ -2,16 +2,20 @@ import type { Env } from '../../auth';
 
 export const onRequestGet: PagesFunction<Env> = async (context) => {
     try {
-        const { env, params } = context;
+        const { env, params, request } = context;
         const workflowId = params.workflow_id;
-        console.log("Download ID>>>", workflowId)
+        const url = new URL(request.url);
+        const version = url.searchParams.get('version');
+        console.log("Download ID>>>", workflowId, "Download Version>>>", version)
         const workflowResult = await env.D1.prepare(
-            'SELECT file_content FROM yaml_files WHERE id = ?'
-        ).bind(workflowId).first();
+            'SELECT file_content FROM yaml_versions WHERE yaml_file_id = ? AND version = ?'
+        ).bind(workflowId, version).first();
 
-        if (!workflowResult) {
+        if (!workflowResult || !version) {
+            console.log("Workflow not found", workflowResult, "Version not found", version);
             return new Response("Workflow not found", { status: 404 });
         }
+        
         const fileContentArrayBuffer = workflowResult.file_content as ArrayBuffer;
         const fileContentUint8Array = new Uint8Array(fileContentArrayBuffer);
         const fileContentDecoder = new TextDecoder("utf-8");
@@ -19,7 +23,7 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
         return new Response(fileContentString, {
             headers: {
                 'Content-Type': 'text/yaml',
-                'Content-Disposition': `attachment; filename="${workflowResult.filename}.yml"`,
+                'Content-Disposition': `attachment; filename="${workflowResult.filename}_${version}.yml"`,
             }
         });
     } catch (error) {
