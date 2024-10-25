ba6ca3aba26cc829543c7bc99853d7938bcc5078
green-dalii <654534332@qq.com>
2024-10-25 16:29:20 +0800

    Add Breadcrumbs navigation component

---
+++ src/content/blog/en/test.md
@@ -0,0 +1,14 @@
+---
+title: "Test"
+pubDate: 2024-10-25
+description: "Test blog description"
+author: "Greener-Dalii"
+occupation: "Founder of Diflowy Community"
+heroImage: "/src/data/blog/test/HEROIMAGE.jpg"
+heroImageAlt: 'Blog Hero Image'
+tags: ["Diflowy", "official" ]
+---
+
+## Test
+
+this is a test blog for diflowy community.
---
+++ src/content/config.ts
@@ -0,0 +1,27 @@
+// 从 `astro:content` 导入辅助工具
+// import { string } from "astro/zod";
+import { z, defineCollection, reference } from "astro:content";
+// import { getCollection } from "astro:content";
+
+// 为每一个集合定义一个 `type` 和 `schema`
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    occupation: z.string(),
+    heroImage: z.string(),
+    heroImageAlt: z.string(),
+    tags: z.array(z.string()),
+  })
+});
+
+// 导出一个单独的 `collections` 对象来注册你的集合
+export const collections = {
+  blog: blogCollection,
+};
+
+// 获取Blog所有图片
+export const blogImages = import.meta.glob("/src/data/blog/*/*.jpg", { eager: true });
